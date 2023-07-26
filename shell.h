176990d35631b2484d9ad8c2308db8aab9488dfd
Nyenzo <nyenzoisabwa@gmail.com>
2023-07-27 00:32:44 +0300

    simple shell

--- shell.h
+++ shell.h
@@ -1,6 +1,48 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+/* Global environemnt */
+extern char **environ;
+/* Global program name */
+extern char *name;
+/* Global history counter */
+extern int hist;
+
+/**
+ * struct list_s - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_s.
+ */
+typedef struct list_s
+{
+	char *dir;
+	struct list_s *next;
+} list_t;
+
+/**
+ * struct builtin_s - A new struct type defining builtin commands.
+ * @name: The name of the builtin command.
+ * @f: A function pointer to the builtin command's function.
+ */
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(char **argv, char **front);
+} builtin_t;
+
 /*
     * AUTHOURS - David Ihaji and Peter Nyenzo
     * Header file for the shell team project
